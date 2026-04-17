dca0e842503ab92d967be55ddbaac9da6b7d9767
202431045MN <202431045MN@gmail.com>
2026-04-17 10:49:03 +0200

    input and user manual

--- subtask_2.cpp
+++ subtask_2.cpp
@@ -1,8 +1,23 @@
-// 202431045
-#include <iostream>
-using namespace std;
-
-int main() {
-    int tempnum1;
-	int tempnum2;
-	int tempnum3;
+	// main menu and user guide 
+	cout <<" ________________________________________________________________________\n";
+	cout <<" MAGWINYA   MAGIC   FRYING   SYSTEM\n";
+	cout <<" ________________________________________________________________________\n";
+	cout <<" USER GUIDE:\n";
+	cout <<"Please enter three readings respectively, note that readings should be entered five minutes apart:\n";
+	cout <<"This system only accepts whole numbers DO NOT USE FLOATING POINT NUMBERS:\n";
+	cout <<" You may proceed...\n";
+	
+	
+	// prompting te user to enter temperatures as per the user guide
+	cout <<" CURRENT TIME :   00:00\n";  // waiting time starting point in hours and minutes format
+	cout <<"Please enter the first temperature reading\n";
+	cin  >> tempnum1;
+	
+	cout <<"PlEASE WAIT FIVE MINUTES BEFORE RECORDING YOUR READING... \n";
+	cout <<" CURRENT TIME :   00:05\n";  // waiting time after entering the first temperature in hours and minutes format
+	cout <<"Please enter the second temperature reading\n";
+	cin  >> tempnum2;	
+	
+	//calculating tempeature difference
+    int newtemp1 = tempnum2 - tempnum1;
+    int newtemp2 = tempnum3 - tempnum2;
